265f177b37b95e3b066d788bc575fc59fc143e53
chaky606 <chaky1606@gmail.com>
2022-12-20 22:08:39 +0700

    List Data + class quote.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -14,10 +15,10 @@ class QuoteList extends StatefulWidget {
 class _QuoteListState extends State<QuoteList> {
   
   //create list to store quotes text
-  List<String> quotes =[
-    'Be yourslf; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The trust is rarely pure and never simple'
+  List<Quote> quotes =[
+    Quote(text: 'Be yourself; everyone else is already taken', author: 'Oscar'),
+    Quote(text: 'I have nothing to declare except my genius', author: 'Oscar'),
+    Quote(text: 'The trust is rarely pure and never simple', author: 'Oscar')
   ];
   
   @override
@@ -31,7 +32,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         //return text from list
-        children: quotes.map((quote) => Text(quote)).toList(),
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
       ),
     );
   }
